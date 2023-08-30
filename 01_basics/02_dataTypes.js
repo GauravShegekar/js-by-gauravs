13c8d2a5152540a0d663b02b293dda7c79bbe75f
Gaurav Shegekar <143530289+GauravShegekar@users.noreply.github.com>
2023-08-30 12:44:16 +0000

    dattype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//code readability should be high
+
+//data types
+let name = "hitesh"
+let age = 10
+let isLoggedIn = false
+
+//number=> 255
+//bigInt
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol =>unique
+
+//object
+
+
+console.log(typeof"hitesh");// undefined
+console.log(typeof null); //on=bject
