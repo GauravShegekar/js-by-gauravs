c2f243dfd4dd99c150758caf6d88cedf6c81530f
Gaurav Shegekar <143530289+GauravShegekar@users.noreply.github.com>
2023-08-31 17:03:50 +0000

    conversion op

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,10 @@
 let score = "33abc"
-
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 //"33" => 33
 //"33abc" => NaN(not a number)
@@ -13,7 +12,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1 => true; 0 =>false
 // "" => false
@@ -22,5 +21,34 @@ console.log(booleanIsLoggedIn)
 
 let sNumber = 33;
 let stNumber = String(sNumber);
-console.log(sNumber);
-console.log(typeof stNumber);
+// console.log(sNumber);
+// console.log(typeof stNumber);
+
+
+//************************* Operations ******************************************//
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 %  3);
+
+console.log(+true)
